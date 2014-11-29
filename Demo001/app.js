d1d8af24cc567a26a622275a0e193fd940386615
Drew Prescott <code@doprogress.com>
2014-11-29 13:04:04 -0600

    Additional changes
    Updated custom filters, etc.

--- Demo001/app.js
+++ Demo001/app.js
@@ -5,9 +5,10 @@ app.controller('header', ['$scope', function($scope) {
 
   $scope.introductions = [{value: true, story: "There can be only one..."},{value: false, story: "to be, or not to be, that is the question..."},{value: true, story: "Or maybe they is something else at test here."}];
 
-  $scope.$watch('myData.val', function(newval){
-    $scope.myData.toolong = newval.length > 3;
+  $scope.$watch('myData.val', function(x){
+    $scope.myData.toolong = x.length > 3;
   });
+
 }]);
 
 app.controller('body', ['$scope', function($scope){
@@ -17,10 +18,14 @@ app.controller('body', ['$scope', function($scope){
                     {name: "landon", age: 1, happy: true}];
 
   $scope.makeHappy = function(indx){
-    $scope.dataArr[indx].happy = false;
-    $scope.digit = indx;
 
+      $scope.$watch('dataArr', function(indx){
+        $scope.dataArr.shift(indx);
+      });
+
+    console.log($scope.dataArr);
   };
+
 }]);
 
 
@@ -46,3 +51,31 @@ app.controller('bodyThree', ['$scope', function($scope){
   $scope.family = {people: [{name: "Mallory", age: 26}, {name: "Drew", age: 27}, {name: "Landon", age: 1.5}]};
 
 }]);
+
+app.filter('charFilter', function(){
+  return function(input, length){ //input is the name expected, length is the the paramenter of what's expected
+    if (!length){
+      length = 10;
+    }
+    if(!input){
+      return '';
+    }
+
+    if (input.length == length){
+      return input;
+    } else {
+      return input.substring(1, length) + '...';
+    }
+  };
+});
+
+app.filter('changeName', function(){
+  return function(text){
+    if (text == 'Prescott'){
+      return text.toUpperCase();
+    } else {
+      return text;
+    }
+
+  };
+});
